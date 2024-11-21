b816b407190935500e124c5cc4f7103c2e79bc73
Atur Aritonang <mangatur.aritonang@gmail.com>
2024-11-21 10:36:54 +0700

    Add List & Details

--- XsisPos/XsisPos.Web/Controllers/CategoryController.cs
+++ XsisPos/XsisPos.Web/Controllers/CategoryController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using XsisPos.Dto;
 
 namespace XsisPos.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private static List<CategoryDto> _list = new List<CategoryDto>() {
+            new CategoryDto()
+            {
+                Id = 1,
+                Initial = "Food",
+                Name = "Fresh Food",
+            },
+            new CategoryDto()
+            {
+                Id = 2,
+                Initial = "Drink",
+                Name = "Fresh Drink",
+            },
+        };
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(_list);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(_list.Where(o => o.Id == id).FirstOrDefault());
         }
     }
 }
