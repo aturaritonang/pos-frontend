6494ef345c18226d4de29e8ef5ba6a504ae024eb
Atur Aritonang <mangatur.aritonang@gmail.com>
2024-11-28 15:30:28 +0700

    Category View List connected to backend

--- XsisPos/XsisPos.Web/Controllers/CategoryController.cs
+++ XsisPos/XsisPos.Web/Controllers/CategoryController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
 using XsisPos.Dto;
+using XsisPos.Web.Helpers;
 
 namespace XsisPos.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly HttpClientHelper _httpClientHelper;
+        public CategoryController(IConfiguration configuration)
+        {
+            _httpClientHelper = new HttpClientHelper(configuration);
+        }
+
         private static List<CategoryDto> _list = new List<CategoryDto>() {
             new CategoryDto()
             {
@@ -25,9 +34,14 @@ namespace XsisPos.Web.Controllers
             return View();
         }
 
-        public IActionResult List()
+        public async Task<IActionResult> List()
         {
-            return View(_list);
+            using (var response = await _httpClientHelper.GetHttpClient().GetAsync("categories"))
+            {
+                var apiResponse = await response.Content.ReadAsStringAsync();
+                return View(JsonConvert.DeserializeObject<List<CategoryDto>>(apiResponse));
+            }
+            return View(new List<CategoryDto>());
         }
 
         public IActionResult Details(int id)
---
+++ XsisPos/XsisPos.Web/Helpers/HttpClientHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace XsisPos.Web.Helpers
+{
+    public class HttpClientHelper
+    {
+        private readonly IConfiguration _configuration;
+        private string _baseUrl;
+
+        public HttpClientHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _baseUrl = _configuration.GetValue<string>("BackEndUrl");
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri($"{_baseUrl.ToString()}/");
+            //dynamic _token = HttpContext.Current.Session["token"];
+            //if (_token == null) throw new ArgumentNullException(nameof(_token));
+            //MyHttpClient.DefaultRequestHeaders.Add("Authorization", String.Format("Bearer {0}", _token.AccessToken));
+            return httpClient;
+        }
+    }
+}
